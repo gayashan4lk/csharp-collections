b18c48b7b145dbf1bf3724ee8994512b03a16c58
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-25 17:06:28 +0530

    enumerating an array

--- DaysOfWeek/Program.cs
+++ DaysOfWeek/Program.cs
@@ -1,6 +1,16 @@
 ﻿string[] daysOfWeek = new string[] { "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Satureday", "Sunday" };
 
+Console.WriteLine("Which day you want to display ?");
+
+int count = 0;
+
 foreach (string day in daysOfWeek)
 {
-    Console.WriteLine(day);
+    Console.WriteLine($"{day} = {++count}");
 }
+
+Console.WriteLine("Enter you input :");
+int iDay = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"You have choosen : {daysOfWeek[--iDay]}");
+
