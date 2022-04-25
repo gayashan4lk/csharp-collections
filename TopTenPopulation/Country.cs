dc78a5a02d90a4ea4159fd105aa8874fef68c1df
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-25 23:10:46 +0530

    begin chapter 02

---
+++ TopTenPopulation/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopTenPopulation
+{
+    public class Country
+    {
+        public Country(string name, string code, string region, int population)
+        {
+            Name = name;
+            Code = code;
+            Region = region;
+            Population = population;
+        }
+
+        public string Name { get; private set; }
+        public string Code { get; private set; }
+        public string Region { get; private set; }
+        public int Population { get; private set; }
+
+    }
+}
---
+++ TopTenPopulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using TopTenPopulation;
+
+string filePath = @"C:\Users\ErangaGayashanBISTEC\source\learning\csharp-collections\Datasources\Pop by Largest Final.csv";
+
+CsvReader reader = new CsvReader(filePath);
+
+Country[] countries = reader.ReadFirstNCountries(10);
+
+foreach (Country country in countries)
+{
+    Console.WriteLine($"{country.Name} :{country.Population}");
+}
