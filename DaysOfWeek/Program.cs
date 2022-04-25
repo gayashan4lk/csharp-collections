b9a06bcefe5e17ab071e5a506f87cb219795e6f8
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-25 13:22:19 +0530

    Add project files.

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] daysOfWeek = new string[] { "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Satureday", "Sunday" };
+
+foreach (string day in daysOfWeek)
+{
+    Console.WriteLine(day);
+}
