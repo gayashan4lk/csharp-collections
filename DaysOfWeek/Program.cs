6070f0a1979187a087a671955aa94827a4148bf0
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-25 17:24:46 +0530

    replacing array elements

--- DaysOfWeek/Program.cs
+++ DaysOfWeek/Program.cs
@@ -1,16 +1,20 @@
-﻿string[] daysOfWeek = new string[] { "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Satureday", "Sunday" };
-
-Console.WriteLine("Which day you want to display ?");
-
+﻿string[] daysOfWeek = new string[] { "Monday", "Tuesday", "Wedssdfsdsfsfsdfsday", "Thursday", "Friday", "Satureday", "Sunday" };
 int count = 0;
 
+Console.WriteLine("First Array");
 foreach (string day in daysOfWeek)
-{
-    Console.WriteLine($"{day} = {++count}");
-}
+    Console.WriteLine($"{++count} - {day}");
 
+daysOfWeek[2] = "Wednsday";
+count = 0;
+Console.WriteLine("Second Array");
+foreach (string day in daysOfWeek)
+    Console.WriteLine($"{++count} - {day}");
+
+
+/*
 Console.WriteLine("Enter you input :");
 int iDay = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"You have choosen : {daysOfWeek[--iDay]}");
+Console.WriteLine($"You have choosen : {daysOfWeek[--iDay]}");*/
 
