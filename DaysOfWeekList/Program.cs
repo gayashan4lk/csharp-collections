f6443c25a41015132f9b46d62aef00a58df0f6b3
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-26 10:28:06 +0530

    intro to List<T>

---
+++ DaysOfWeekList/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DaysOfWeekList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*List<string> daysOfWeek = new List<string>();
+            daysOfWeek.Add("Monday");
+            daysOfWeek.Add("Tuesday");*/
+
+            // Collection Intializer -> still Add() is called!
+            List<string> daysOfWeek = new List<string>
+            {
+                "Monday",
+                "Tuesday",
+                "Wednsday",
+                "Thursday",
+                "Friday",
+                "Satureday",
+                "sunday"
+            };
+
+            foreach (var day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+        }
+    }
+}
