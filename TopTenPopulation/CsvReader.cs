00ee1076140256a6e611829355398d8242a1be91
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-26 09:01:27 +0530

    read CSV file

--- TopTenPopulation/CsvReader.cs
+++ TopTenPopulation/CsvReader.cs
@@ -17,7 +17,32 @@ namespace TopTenPopulation
 
         public Country[] ReadFirstNCountries(int nCountries)
         {
-            return null;
+            Country[] countries = new Country[nCountries];
+
+            using (StreamReader sr = new StreamReader(this._csvFilePath))
+            {
+                sr.ReadLine();
+
+                for (int i = 0; i < nCountries; i++)
+                {
+                    string csvLine = sr.ReadLine();
+                    countries[i] = ReadCountryFromCsvLine(csvLine);
+                }
+            }
+
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(new char[] { ',' });
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
         }
     }
 }
